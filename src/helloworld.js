93957f1871630b8bcbb4a311e307163420ddf4ce
oudanzhou <oudanzhou@medlinker.com>
2022-04-01 15:33:16 +0800

    babel-loader 处理js,将es6转化为es5

--- src/helloworld.js
+++ src/helloworld.js
@@ -5,3 +5,17 @@ export function helloworld() {
 export function helloTest() {
   return 'Hello test'
 }
+
+export const test = () => {
+  console.log('test', this?.a)
+}
+
+export const test2 = () => {
+  console.log('test', this?.a)
+}
+
+export function test3 () {
+  return () => {
+    console.log('test', this?.c)
+  }
+}
--- webpack.config.js
+++ webpack.config.js
@@ -14,11 +14,15 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
     filename: '[name].js', // 占位符默认main
   },
-  mode: 'development', // 'development' || 'production'
+
+  mode: 'none', // 'development' || 'production' || 'none'
   // loaders: 处理webpack原生不能处理的文件（模块），原生只支持js和json
   module: {
     rules: [
-      {}
+      {
+        test: /\.js$/,
+        use: 'babel-loader'
+      }
     ]
   }
 }
