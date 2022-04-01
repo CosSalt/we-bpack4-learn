390c6f6f41e24d0dafc06df485c82d503a72358c
oudanzhou <oudanzhou@medlinker.com>
2022-04-01 22:49:03 +0800

    feat: style-loader css-loader

--- src/search.js
+++ src/search.js
@@ -1,1 +1,13 @@
-document.write('search page')
+import React from 'react'
+import ReactDom from 'react-dom'
+import './search.css'
+
+const Search  = () => {
+  return (
+    <div className='search-text'>
+      Search Text
+    </div>
+  )
+}
+
+ReactDom.render(<Search />, document.getElementById('root'))
