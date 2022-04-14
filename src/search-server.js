2ccd4a200b97847559558b9aafbdd0bf0a3aa2e5
odanzhou <odanzhou@gmail.com>
2022-04-14 16:04:07 +0800

    重新组织代码结构

--- src/search-server.js
+++ src/search-server.js
@@ -1,42 +1,4 @@
-// import React, { useState } from 'react'
-// import logo from './images/logo.jpg'
-// import TestModule from './test'
-// import { common } from './common'
-// import lagetNumber from 'odan-large-number'
-// import './search.less'
-
 const React = require('react')
-const logo = require('./images/logo.jpg')
-const lagetNumber = require('odan-large-number')
-require('./search.less')
-const { useState } = React
-// 用useState 会报错
-const Search = () => {
-  // const [state, setState] = useState(0)
-  // const onClick = () => setState(x => x+1)
-  const state = 1
-  const onClick = () => {} 
-  return (
-    <div className='search-text test'>
-      <div>state: {state}</div>
-      <button onClick={onClick}>click12</button>
-      <button onClick={onClick}>动态 import</button>
-      { lagetNumber(999, 999)}
-      <div>
-        1
-        <div>11
-          <div>111</div>
-          <div>112</div>
-        </div>
-        <div>12
-          <div>121</div>
-          <div>122</div>
-        </div>
-      </div>
-      <img src={logo}></img>
-      Search Text312
-    </div>
-  )
-}
+const { default: SearchComponent } =  require('./components/search')
 
-module.exports = <Search />
+module.exports = <SearchComponent />
