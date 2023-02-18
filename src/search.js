cd88a80a8e620f11cc5b10ff29561b908a1a15ce
odanzhou <odanzhou@gmail.com>
2023-02-19 00:10:58 +0800

    feat: 处理less 文件

--- src/search.js
+++ src/search.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDom from 'react-dom'
-import './search.css'
+import './search.less'
 
 const Search = () => {
   return <div className="search-text">Search Text</div>
