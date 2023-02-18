a21371e44a634c7f210e034bc65129344ee01048
odanzhou <odanzhou@gmail.com>
2023-02-19 00:00:11 +0800

    feat: babel && 解析react

--- src/search.js
+++ src/search.js
@@ -1,3 +1,8 @@
-export const search = () => {
-  return 'search'
+import React from 'react'
+import ReactDom from 'react-dom'
+
+const Search = () => {
+  return <div>Search Text</div>
 }
+
+ReactDom.render(<Search/>, document.getElementById('root'))
