6eddfa2d020b4891cc0362ef333952de58a438f2
odanzhou <odanzhou@gmail.com>
2023-02-19 00:44:39 +0800

    feat: 文件监听构建，两种方式

--- webpack.config.js
+++ webpack.config.js
@@ -48,5 +48,16 @@ module.exports = {
         use: 'file-loader'
       }
     ]
+  },
+  // 文件变化监听，默认false
+  watch: true,
+  // 只有开启了监听模式watchOptions才有意义
+  watchOptions: {
+    // 不监听的文件或文件见，默认为空，支持正则匹配
+    ignored: /node_modules/,
+    // 监听到变化后会等300ms再去执行编译，默认300ms
+    aggregateTimeout: 300,
+    // 判断文件是否变化是通过不停询问系统制定文件是否发生变化实现的，默认每秒问 1000 次
+    poll: 1000
   }
 }
