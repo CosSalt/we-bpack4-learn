86c2222c0722dec419390da6cc427b9d4722044e
odanzhou <odanzhou@gmail.com>
2023-02-24 00:05:02 +0800

    chore: eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,22 @@
 module.exports = {
   "parser": "esprima",
+  "parserOptions": {
+    "ecmaVersion": 11,
+    "sourceType": "module",
+    "ecmaFeatures": {
+      "jsx": true
+    }
+  },
   // "extends": "airbnb", // 继承多个用数组
-  // "rules": {
-  //   "eol-last": "off",
-  //   "semi": "off",
-  // },
-  // "env": {
-  //   "browser": true,
-  //   "node": true,
-  // }
+  "extends": "eslint:recommended",
+  "rules": {
+    "eol-last": "off",
+    "semi": "off",
+    "es6": true,
+    "no-console": 0,
+  },
+  "env": {
+    "browser": true,
+    "node": true,
+  }
 }
