9d4e79897299632e4077b6fdaa0ed540404e1852
odanzhou <odanzhou@gmail.com>
2023-02-26 22:35:30 +0800

    chore: 屏蔽eslint

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -73,7 +73,7 @@ const rules = [
     test: /\.js$/,
     use: [
       'babel-loader',
-      'eslint-loader',
+      // 'eslint-loader',
     ]
   },
   {
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,1 +1,2 @@
+/* eslint-disable */
 export const test = (key = '') => 'test' + key
